a6a04c31bce05cc0562693072663bde4d461fd6e
Sheth <astha.sheth@capgemini.com>
2023-07-11 10:51:32 +0530

    react router v6.4.1

---
+++ React-Router-6.4/src/main.jsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import ErrorPage from "./ErrorPage";
+import Root from "./routes/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
